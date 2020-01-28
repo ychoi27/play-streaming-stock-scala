8cb04d2c1f9503bab0fb42fcdd1cf551e19b1dfa
Yunhu Choi <ck43789@gmail.com>
2020-01-27 22:11:04 -0500

    add the MyStock serializer

--- app/models/MyStock.scala
+++ app/models/MyStock.scala
@@ -1,5 +1,18 @@
 package models
 
-object MyStock {}
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
 
 case class MyStock(symbol: String, price: BigDecimal)
+
+object MyStock {
+  implicit val locationWrites = new Writes[MyStock] {
+    def writes(stock: MyStock) =
+      Json.obj("symbol" -> stock.symbol, "price" -> stock.price)
+  }
+
+  implicit val residentReads: Reads[MyStock] = (
+    (JsPath \ "symbol").read[String] and
+      (JsPath \ "price").read[BigDecimal]
+  )(MyStock.apply _)
+}
